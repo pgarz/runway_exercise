abe237c72609452f7b27608c92d96d3f7fa07cc5
Pedro Garzon <pedropgarzon@gmail.com>
2019-07-01 12:30:01 -0700

    trigger build 4

--- make_test_post_request.py
+++ make_test_post_request.py
@@ -9,6 +9,6 @@ image_path = './blurred_sharp/blurred/248.png'
 with open(image_path, 'rb') as fd:
     b64data = base64.b64encode(fd.read())
 
-files = {'blurred_image': ('248.png', b64data, 'application/json')}
+files = {'blurred_image': b64data}
 headers = {'Content-type': 'application/json'}
 requests.post(url, files=files, headers=headers)
